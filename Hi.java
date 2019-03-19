6065afcaf6beb59f5f4859aeb5b741336abc2805
leesangho235 <48707965+leesangho235@users.noreply.github.com>
2019-03-19 15:20:58 +0900

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,25 @@
+package java01; //폴더개념 => 맨위에!
+
+public class Hi {
+
+	public static void main(String[] args) {
+		// 나의 첫 자바 프로그램(주석,comment)
+		// 프로그램의 기본 입력(키보드)/출력(모니터)
+		// 키보드 System.in 
+		// 모니터 System.out
+		// 자동완선 밑에 System.out.print 이런거 콘솔 시작할 때 자동적 기능 컨트롤+SpaceBar
+		// 모니터에 프린트: syso+ 자동완성
+		System.out.println("나는 자동완성..!!!");
+		System.out.println("안녕하세요.. 자바에 오신 것을 환영합니다");
+		System.out.println("나는 세번째 줄 프린트...!!");          // println 에서 ln은 엔터키이다. 
+		// 저장 : 컨트롤 +s
+		// 실행 : run (컨트롤 f11)
+		//
+	}
+
+}
+
+
+
+
+
